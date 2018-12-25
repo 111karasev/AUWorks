1bdf7ee561033f4ba5c3f0b3e36800441eccbbdf
111karasev <43390123+111karasev@users.noreply.github.com>
2018-12-25 23:48:59 +0300

    Create lab7.py

---
+++ lab7.py
@@ -0,0 +1,35 @@
+import numpy as np
+import math
+def evk(a, b):
+    if(b==0):
+        return a
+    else:
+        return evk(b, a%b)
+
+def isNumberPrime(a):
+    if (a%2==0):
+        return False
+    i=3
+    while i*i<a:
+        if(a%i==0):
+            return False
+        i+=2
+    return True
+
+def str_base(number, base):
+    nums=[]
+    while number!=0:
+        mod=number%base
+        if(mod>=10):
+            nums.append(chr(ord('A')+mod-10))
+        else:
+            nums.append(mod)
+        number=number//base
+    nums.reverse()
+    return ''.join(nums)
+
+def evk_up(a, b):
+    if a==0:
+        return (b, 0, 1)
+    evk, x, y=evk_up(b%a, a)
+    return (evk, y-(b//a)*x, x)
